4505076454971e0f49d2f4c770ec5018131a00e1
emilowhits <50845354+emilowhits@users.noreply.github.com>
2025-05-12 16:03:51 +0200

    Animationer virker, skal bare implementeres anderledes

--- SpilProg2 MirrorDB/Assets/Scripts/Animations/RedEnemyAnimation.cs
+++ SpilProg2 MirrorDB/Assets/Scripts/Animations/RedEnemyAnimation.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class Animation : MonoBehaviour
+public class RedEnemyAnimation : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GetComponent<Animator>().Play("RedEnemyAnimation");
+        }
     }
 }
