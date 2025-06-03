3a42ad646759d51d756a9064bb06d58b0b4b945b
emilowhits <50845354+emilowhits@users.noreply.github.com>
2025-06-03 04:14:03 +0200

    it is the work yay
    🐶

--- SpilProg2 MirrorDB/Assets/Submit.cs
+++ SpilProg2 MirrorDB/Assets/Submit.cs
@@ -4,21 +4,25 @@ public class Submit : MonoBehaviour
 {
 
     public EndOfGameUIManager EOG;
+    public ScoreDisplayManager SDM;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         EOG = GameObject.Find("EndOfGame UI Manager").GetComponent<EndOfGameUIManager>();
+        SDM = GameObject.Find("ScoreDisplay Manager").GetComponent<ScoreDisplayManager>();
     }
 
     public void Submit1()
     {
         EOG.OnSubmitName1();
+        SDM.ShowScoreboard();
     }
 
     public void Submit2()
     {
         EOG.OnSubmitName2();
+        SDM.ShowScoreboard2();
     }
 
     // Update is called once per frame
